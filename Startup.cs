3805f13135e5f7a17729040b7b48aba5e1262218
Ngoc Tran <ngoctranfire@gmail.com>
2015-11-11 20:49:38 -0800

    First Commit

---
+++ Startup.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace EmptyApplication
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // Add the platform handler to the request pipeline.
+            app.UseIISPlatformHandler();
+            
+            app.Run(async (context) =>
+            {
+                Console.WriteLine("Request for " + context.Request.Path);
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
